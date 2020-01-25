3239776930be30f84506a12fb29437a13911b99d
drdr xp <drdr.xp@gmail.com>
2020-01-25 20:48:36 +0800

    complete commands
    -   draft README
    
    -   supported command:
    
        git clone --sub
        git commit --sub
    
        git init --sub
        git fetch --sub
        git merge --sub
        git reset --sub
    
    -   simplify config syntax
    
    -   add sample bashrc
    
    -   rename .gift-state to .gift-refs
    
    -   Add class `Git` to wrap git commands

--- README.md
+++ README.md
@@ -3,3 +3,118 @@
 [![Build Status](https://travis-ci.org/drmingdrmer/gift.svg?branch=master)](https://travis-ci.org/drmingdrmer/gift)
 
 gift is a fine tool working with mult-repo, multi-branch and multi-workdir.
+
+# Install
+
+```
+
+```
+
+# Usage
+
+- A super repo to manage many sub repo
+- Managing dependent repos
+
+In a git working dir:
+clone a sub repo into folder "path/to/subrepo"
+```
+git clone --sub https://github.com/drmingdrmer/gift.git@master path/to/subrepo
+```
+
+- Hacking with sub repo is just the same as working with a standard git repo:
+
+    ```
+    cd path/to/subrepo
+    echo hello > world
+    git add world
+    git commit world -m 'say hi'
+    ```
+
+- Update sub repo changes to super repo
+    <!-- TODO commit message -->
+    ```
+    cd -
+    git commit --sub
+    ```
+
+## Enhanced commands
+
+```
+git clone --sub
+git commit --sub
+
+git init --sub
+git fetch --sub
+git merge --sub
+git reset --sub
+```
+
+
+## Setup sub repo
+
+Setup every sub repo defined in `.gift`,
+fetch from sub repo remote,
+and checkout default branch.
+
+```
+git init --sub
+```
+
+## fetch latest sub repo, WITHOUT update work tree.
+
+```
+git fetch --sub
+
+for each subrepo:
+    git fetch <default-remote>
+
+```
+
+## Update sub repos to latest
+
+```
+git merge --sub
+
+for each subrepo:
+    git merge --ff-only <default-upstream>
+```
+
+## Put sub repo updates to super repo
+
+```
+git commit --sub
+```
+
+## Reset sub repo to the commit super repo expect.
+
+```
+git reset --sub [--soft|--mixed|--keep|--hard]
+
+for each subrepo:
+    git reset super/head
+```
+
+# Comparison to other solution
+
+- git-submodule
+
+- git-subtree
+
+- git-subrepo by xp
+- git-subrepo by https://github.com/ingydotnet/git-subrepo
+
+#   Name
+
+x
+
+#   Status
+
+This library is considered production ready.
+
+#   Description
+
+
+
+#   Author
+
+Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>
--- test_gift.py
+++ test_gift.py
@@ -4,16 +4,21 @@
 
 import imp
 import os
+import shutil
 import unittest
 
 gift = imp.load_source('gift', './gift')
 
 cmdx = gift.cmdx
+cmdout = gift.cmdout
+cmd0 = gift.cmd0
 read_file = gift.read_file
 write_file = gift.write_file
 pj = gift.pj
 dd = gift.dd
 _bytes = gift._bytes
+
+Git = gift.Git
 
 
 # root of this repo
@@ -22,19 +27,33 @@ this_base = os.path.dirname(__file__)
 giftp = pj(this_base, "gift")
 origit = "git"
 
+emptyp = pj(this_base, "testdata", "empty")
 superp = pj(this_base, "testdata", "super")
 subbarp = pj(this_base, "testdata", "super", "foo", "bar")
 subwowp = pj(this_base, "testdata", "super", "foo", "wow")
+bargitp = pj(this_base, "testdata", "bargit")
+barp = pj(this_base, "testdata", "bar")
 
 
-class TestGift(unittest.TestCase):
+def _clean_case():
+    for d in ("empty", ):
+        p = pj(this_base, "testdata",        d)
+        cmdx(origit, "reset", "--hard",     cwd=p)
+        cmdx(origit, "clean", "-dxf",        cwd=p)
+
+    force_remove(pj(this_base, "testdata", "empty", ".git"))
+    force_remove(pj(this_base, "testdata", "super", ".git"))
+    force_remove(barp)
+    cmdx(origit, "reset", "testdata", cwd=this_base)
+    cmdx(origit, "checkout", "testdata", cwd=this_base)
+    cmdx(origit, "clean", "-dxf", cwd=this_base)
+
+
+class BaseTest(unittest.TestCase):
 
     def setUp(self):
 
-        force_remove(pj(this_base, "testdata", "super", ".git"))
-        cmdx("git", "reset", "testdata", cwd=this_base)
-        cmdx("git", "checkout", "testdata", cwd=this_base)
-        cmdx("git", "clean", "-dxf", cwd=this_base)
+        _clean_case()
 
         # .git can not be track in a git repo.
         # need to manually create it.
@@ -42,17 +61,128 @@ class TestGift(unittest.TestCase):
                    "gitdir: ../supergit")
 
     def tearDown(self):
-        force_remove(pj(this_base, "testdata", "super", ".git"))
-        cmdx("git", "reset", "testdata", cwd=this_base)
-        cmdx("git", "checkout", "testdata", cwd=this_base)
-        cmdx("git", "clean", "-dxf", cwd=this_base)
+        if os.environ.get("GIFT_NOCLEAN", None) == "1":
+            return
+        _clean_case()
 
-    def test_init(self):
+
+class TestGit(BaseTest):
+
+    def test_blob_new(self):
+        write_file(pj(superp, "newblob"), "newblob!!!")
+        g = Git(start_dir=superp)
+        blobhash = g.blob_new("newblob")
+
+        content = cmd0(origit, "cat-file", "-p", blobhash, cwd=superp)
+        self.assertEqual("newblob!!!", content)
+
+    def test_add_tree(self):
+
+        g = Git(start_dir=superp)
+
+        roottreeish = g.get_tree("HEAD")
+
+        dd(cmdx(origit, "ls-tree", "87486e2d4543eb0dd99c1064cc87abdf399cde9f", cwd=superp))
+        self.assertEqual("87486e2d4543eb0dd99c1064cc87abdf399cde9f", roottreeish)
+
+        # shallow add
+
+        newtree = g.tree_add_obj(roottreeish, "nested", roottreeish)
+
+        files = cmdout(origit, "ls-tree", "-r", "--name-only", newtree, cwd=superp)
+        self.assertEqual([
+            ".gift",
+            "imsuperman",
+            "nested/.gift",
+            "nested/imsuperman",
+        ], files)
+
+        # add nested
+
+        newtree = g.tree_add_obj(newtree, "a/b/c/d", roottreeish)
+
+        files = cmdout(origit, "ls-tree", "-r", "--name-only", newtree, cwd=superp)
+        self.assertEqual([
+            ".gift",
+            "a/b/c/d/.gift",
+            "a/b/c/d/imsuperman",
+            "imsuperman",
+            "nested/.gift",
+            "nested/imsuperman",
+        ], files)
+
+        # replace nested
+
+        newtree = g.tree_add_obj(newtree, "a/b/c", roottreeish)
+
+        files = cmdout(origit, "ls-tree", "-r", "--name-only", newtree, cwd=superp)
+        self.assertEqual([
+            ".gift",
+            "a/b/c/.gift",
+            "a/b/c/imsuperman",
+            "imsuperman",
+            "nested/.gift",
+            "nested/imsuperman",
+        ], files)
+
+        # replace a blob with tree
+
+        newtree = g.tree_add_obj(newtree, "a/b/c/imsuperman", roottreeish)
+
+        files = cmdout(origit, "ls-tree", "-r", "--name-only", newtree, cwd=superp)
+        self.assertEqual([
+            ".gift",
+            "a/b/c/.gift",
+            "a/b/c/imsuperman/.gift",
+            "a/b/c/imsuperman/imsuperman",
+            "imsuperman",
+            "nested/.gift",
+            "nested/imsuperman",
+        ], files)
+
+        # replace a blob in mid of path with tree
+
+        newtree = g.tree_add_obj(newtree, "nested/imsuperman/b/c", roottreeish)
+
+        files = cmdout(origit, "ls-tree", "-r", "--name-only", newtree, cwd=superp)
+        self.assertEqual([
+            ".gift",
+            "a/b/c/.gift",
+            "a/b/c/imsuperman/.gift",
+            "a/b/c/imsuperman/imsuperman",
+            "imsuperman",
+            "nested/.gift",
+            "nested/imsuperman/b/c/.gift",
+            "nested/imsuperman/b/c/imsuperman",
+        ], files)
+
+
+class TestGift(BaseTest):
+
+    def test_clone_sub(self):
+        cmdx(giftp, "init", cwd=emptyp)
+        cmdx(giftp, "clone", "--sub", "../bargit", "path/to/bar", cwd=emptyp)
+        self._gitoutput([giftp, "ls-files"], [
+            ".gift",
+            ".gift-refs",
+            "path/to/bar/bar",
+        ], cwd=emptyp)
+
+        self._fcontent("dirs:\n  path/to/bar: ../bargit\n", emptyp, ".gift")
+        self._fcontent("\n".join([
+                "- - path/to/bar",
+                "  - 466f0bbdf56b1428edf2aed4f6a99c1bd1d4c8af",
+                "", 
+        ]), emptyp, ".gift-refs")
+        self._fcontent("bar\n", emptyp, "path/to/bar/bar")
+
+
+    def test_init_sub(self):
         self._nofile(subbarp, "bar")
         self._nofile(subwowp, "wow")
 
         for _ in range(2):
-            cmdx(giftp, "initsub", cwd=superp)
+            cmdx(giftp, "init", "--sub", cwd=superp)
 
             self._fcontent("bar\n", subbarp, "bar")
             self._fcontent("wow\n", subwowp, "wow")
@@ -61,8 +191,8 @@ class TestGift(unittest.TestCase):
             self._gitoutput([giftp, "symbolic-ref", "--short", "HEAD"], ["master"], cwd=subwowp)
             self._gitoutput([giftp, "ls-files"], [".gift", "imsuperman"], cwd=superp)
 
-    def test_commit_super(self):
-        cmdx(giftp, "initsub", cwd=superp)
+    def test_commit_in_super(self):
+        cmdx(giftp, "init", "--sub", cwd=superp)
         cmdx(giftp, "add", "foo", cwd=superp)
         cmdx(giftp, "commit", "-m", "add foo", cwd=superp)
 
@@ -75,14 +205,16 @@ class TestGift(unittest.TestCase):
         ],
             cwd=superp)
 
-    def test_commitsub(self):
-        cmdx(giftp, "initsub", cwd=superp)
-        cmdx(giftp, "commitsub", cwd=superp)
+    def test_commit_sub(self):
+        cmdx(giftp, "init", "--sub", cwd=superp)
+        _, out, err = cmdx(giftp, "commit", "--sub", cwd=superp)
+        dd(out)
+        dd(err)
 
         self._gitoutput([giftp, "ls-files"],
                         [
             ".gift",
-            ".gift-state",
+            ".gift-refs",
             "foo/bar/bar",
             "foo/wow/wow",
             "imsuperman",
@@ -95,43 +227,149 @@ class TestGift(unittest.TestCase):
                        "- - foo/wow",
                        "  - 6bf37e52cbafcf55ff4710bb2b63309b55bf8e54",
                        ""]),
-            superp, ".gift-state",
+            superp, ".gift-refs",
         )
+
+    def test_fetch_sub(self):
+
+        cmdx(giftp, "init", "--sub", cwd=superp)
+
+        headhash = self._add_commit_to_bar_from_other_clone()
+
+        # we should fetch and got the latest commit.
+
+        cmdx(giftp, "fetch", "--sub", cwd=superp)
+        fetched_hash = cmd0(giftp, "rev-parse", "origin/master", cwd=subbarp)
+
+        self.assertEqual(headhash, fetched_hash)
+
+    def test_merge_sub(self):
+
+        cmdx(giftp, "init", "--sub", cwd=superp)
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+
+        headhash = self._add_commit_to_bar_from_other_clone()
+
+        # we should fetch and got the latest commit.
+
+        cmdx(giftp, "fetch", "--sub", cwd=superp)
+        cmdx(giftp, "merge", "--sub", cwd=superp)
+        fetched_hash = cmd0(giftp, "rev-parse", "HEAD", cwd=subbarp)
+
+        self.assertEqual(headhash, fetched_hash, "HEAD is updated to latest master")
+
+    def test_reset_sub(self):
+
+        cmdx(giftp, "init", "--sub", cwd=superp)
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+
+        ori_hash = cmd0(giftp, "rev-parse", "HEAD", cwd=subbarp)
+
+        headhash = self._add_commit_to_bar_from_other_clone()
+
+        # we should fetch and got the latest commit.
+
+        cmdx(giftp, "fetch", "--sub", cwd=superp)
+        cmdx(giftp, "merge", "origin/master", cwd=subbarp)
+        fetched_hash = cmd0(giftp, "rev-parse", "HEAD", cwd=subbarp)
+
+        self.assertEqual(headhash, fetched_hash, "HEAD is updated to latest master")
+
+        cmdx(giftp, "reset", "--sub", cwd=superp)
+        reset_hash = cmd0(giftp, "rev-parse", "HEAD", cwd=subbarp)
+        self.assertEqual(ori_hash, reset_hash, "HEAD is reset to original master")
+
+    def _add_commit_to_bar_from_other_clone(self):
+        cmdx(origit, "clone", bargitp, barp)
+
+        write_file(pj(barp, "for_fetch"), "for_fetch")
+        cmdx(origit, "add", "for_fetch", cwd=barp)
+        cmdx(origit, "commit", "-m", "add for_fetch", cwd=barp)
+        cmdx(origit, "push", "origin", "master", cwd=barp)
+
+        headhash = cmd0(origit, "rev-parse", "HEAD", cwd=barp)
+        return headhash
 
     def test_op_in_sub(self):
 
-        cmdx(giftp, "initsub", cwd=superp)
+        cmdx(giftp, "init", "--sub", cwd=superp)
 
-        _, out, _ = cmdx(origit, "rev-parse", "HEAD", cwd=superp)
-        superhash = out[0]
+        superhash = cmd0(origit, "rev-parse", "HEAD", cwd=superp)
         dd(superhash)
 
-        _, out, _ = cmdx(giftp, "rev-parse", "HEAD", cwd=superp)
-        self.assertEqual(superhash, out[0], "gift should get the right super HEAD hash")
+        gift_super_hash = cmd0(giftp, "rev-parse", "HEAD", cwd=superp)
+        self.assertEqual(superhash, gift_super_hash, "gift should get the right super HEAD hash")
 
-        _, out, _ = cmdx(giftp, "rev-parse", "HEAD", cwd=subbarp)
-        barhash = out[0]
+        barhash = cmd0(giftp, "rev-parse", "HEAD", cwd=subbarp)
         self.assertNotEqual(barhash, superhash, "gift should get a different hash in sub dir bar")
 
-        self._add_newbar()
+        self._add_file_to_subbar()
 
-        _, out, _ = cmdx(origit, "rev-parse", "HEAD", cwd=superp)
-        superhash2 = out[0]
+        superhash2 = cmd0(origit, "rev-parse", "HEAD", cwd=superp)
         self.assertEqual(superhash, superhash2, "commit in sub dir should not change super dir HEAD")
 
     def test_populate_super_ref(self):
 
-        cmdx(giftp, "initsub", cwd=superp)
+        cmdx(giftp, "init", "--sub", cwd=superp)
 
-        cmdx(giftp, "commitsub", cwd=superp)
+        # commit --sub should populate super/head
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+        self._check_initial_superhead()
 
-        self._add_newbar()
+        self._add_file_to_subbar()
+        self._remove_super_ref()
+
+        # init --sub should populate super/head
+        cmdx(giftp, "init", "--sub", cwd=superp)
+        self._check_initial_superhead()
+
+    def test_populate_super_ref2(self):
+
+        cmdx(giftp, "init", "--sub", cwd=superp)
+
+        # commit --sub should populate super/head
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+        self._check_initial_superhead()
+
+        head_of_bar = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
+
+        state0 = read_file(pj(superp, ".gift-refs"))
+
+        self._add_file_to_subbar()
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+
+        head1 = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
+        self.assertNotEqual(head_of_bar, head1)
+
+        state1 = read_file(pj(superp, ".gift-refs"))
+        self.assertNotEqual(state0, state1)
+
+        # changing HEAD in super repo should repopulate super/head ref in sub repo
+        cmdx(giftp, "reset", "HEAD~", cwd=superp)
+        head2 = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
+        self.assertNotEqual(head_of_bar, head2)
+
+    def test_super_checkout_should_populate_super_ref(self):
+
+        cmdx(giftp, "init", "--sub", cwd=superp)
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+        head_of_bar = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
+
+        self._add_file_to_subbar()
+        cmdx(giftp, "commit", "--sub", cwd=superp)
+        head_of_bar1 = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
+
+        self.assertNotEqual(head_of_bar, head_of_bar1)
+
+        # changing HEAD in super repo should repopulate super/head ref in sub repo
+        cmdx(giftp, "checkout", "HEAD~", cwd=superp)
+        head_of_bar_after_checkout = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
+
+        self.assertNotEqual(head_of_bar, head_of_bar_after_checkout)
+
+    def _remove_super_ref(self):
         cmdx(giftp, "update-ref", "-d", "refs/remotes/super/head", cwd=subbarp)
         cmdx(giftp, "update-ref", "-d", "refs/remotes/super/head", cwd=subwowp)
-
-        # initsub should populate super/head
-        cmdx(giftp, "initsub", cwd=superp)
-        self._check_initial_superhead()
 
     def _check_initial_superhead(self):
         _, out, _ = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subbarp)
@@ -140,7 +378,7 @@ class TestGift(unittest.TestCase):
         _, out, _ = cmdx(giftp, "rev-parse", "refs/remotes/super/head", cwd=subwowp)
         self.assertEqual("6bf37e52cbafcf55ff4710bb2b63309b55bf8e54", out[0])
 
-    def _add_newbar(self):
+    def _add_file_to_subbar(self):
         write_file(pj(subbarp, "newbar"), "newbar")
         cmdx(giftp, "add", "newbar", cwd=subbarp)
         cmdx(giftp, "commit", "-m", "add newbar", cwd=subbarp)
@@ -164,7 +402,7 @@ class TestGift(unittest.TestCase):
 def force_remove(fn):
 
     try:
-        os.rmdir(fn)
+        shutil.rmtree(fn)
     except BaseException:
         pass
 
