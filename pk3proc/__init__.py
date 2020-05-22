70d35960e0e02bfae194a33af8bfe1464a56f5b8
drdr xp <drdr.xp@gmail.com>
2020-05-22 16:45:15 +0800

    commit subdirs

---
+++ pk3proc/__init__.py
@@ -0,0 +1,43 @@
+"""
+pk3proc is utility to create sub process.
+
+Execute a shell script::
+
+    import pk3proc
+
+    # execute a shell script
+
+    returncode, out, err = pk3proc.shell_script('ls / | grep bin')
+    print returncode
+    print out
+    # output:
+    # > 0
+    # > bin
+    # > sbin
+
+Run a command::
+
+    # Unlike the above snippet, following statement does not start an sh process.
+    returncode, out, err = pk3proc.command('ls', 'a*', cwd='/usr/local')
+
+"""
+
+from .proc import CalledProcessError
+from .proc import ProcError
+from .proc import TimeoutExpired
+from .proc import command
+from .proc import command_ex
+from .proc import shell_script
+from .proc import start_process
+
+__version__ = "0.2.2"
+
+__all__ = [
+    'CalledProcessError',
+    'TimeoutExpired',
+    'ProcError',
+    'command',
+    'command_ex',
+    'shell_script',
+    'start_process',
+]
